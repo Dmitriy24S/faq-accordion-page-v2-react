1e2c412f9f2b1cb23467dad23d1049f3df6473b8
Dmitriy <devilins24@gmail.com>
2021-09-15 20:24:39 +0300

    init project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
+
+function App() {
+  const [questions, setQuestions] = useState(data);
+
+  return (
+    <main>
+      <section className="container">
+        <h3>Questions and answers about login</h3>
+        <section>
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} question={question} />;
+          })}
+        </section>
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Question.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { AiFillCiCircle, AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+const Question = ({ question }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <article className="question" onClick={() => setIsOpen(!isOpen)}>
+      <header>
+        <h4>{question.title}</h4>
+        <button className="btn">
+          {isOpen ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {isOpen && <p>{question.info}</p>}
+    </article>
+  );
+};
+
+export default Question;
